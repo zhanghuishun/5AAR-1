96365a92872f7cf12ed2dd705733241990bec941
Huishun Zhang <345892489@qq.com>
2022-01-13 10:47:21 +0100

    Add UI to settings

--- Assets/Scripts/Settings/InputFieldSubmit.cs
+++ Assets/Scripts/Settings/InputFieldSubmit.cs
@@ -4,24 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
-
+using TMPro;
 
 public class InputFieldSubmit : MonoBehaviour
 {
     //dest start from home:"45.516826", "9.216668"
     public static string[] destinationCoordinates = new string[2] {"45.4827681", "9.2322856"}; // lambrate station,
     public static string[] tabacchiCoordinates = new string[2] {"45.480759", "9.224494"}; //tabacchi near BCL
-    public InputField destinationCord;
-    public InputField tabacchiCord;
-    public Text settingsState;
+    public TMP_InputField destinationCord;
+    public TMP_InputField tabacchiCord;
 
     public void Awake()
     {
-        destinationCord.placeholder.GetComponent<Text>().text = destinationCoordinates[0]+","+destinationCoordinates[1];
-        tabacchiCord.placeholder.GetComponent<Text>().text = tabacchiCoordinates[0]+","+tabacchiCoordinates[1];
+        Debug.Log("print "+destinationCord.placeholder.GetComponent<TextMeshProUGUI>().text);
+        destinationCord.placeholder.GetComponent<TextMeshProUGUI>().text = destinationCoordinates[0]+","+destinationCoordinates[1];
+        tabacchiCord.placeholder.GetComponent<TextMeshProUGUI>().text = tabacchiCoordinates[0]+","+tabacchiCoordinates[1];
         DontDestroyOnLoad(transform.gameObject);
     }
-    public void LockInput(string[] coordinates, InputField inputField)
+    public void LockInput(string[] coordinates, TMP_InputField inputField)
     {
         coordinates = inputField.text.Split(',');
     }
