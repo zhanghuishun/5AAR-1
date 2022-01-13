9b1347fb066854aa4ecc515418a2a52ce68e543f
Leonardo Scapolan <79846644+LeonardoScapolanMI@users.noreply.github.com>
2022-01-13 18:39:09 +0100

    TTS will now be always in english

--- Assets/Scripts/TTS/TTSController.cs
+++ Assets/Scripts/TTS/TTSController.cs
@@ -2,11 +2,12 @@ using GoodEnough.TextToSpeech;
 
 public class TTSController
 {
+    private static string defaultLanguage = "en";
 
     public static void Speak(string text)
     {
 #if UNITY_IOS
-        TTS.Speak(text);
+        TTS.Speak(text, defaultLanguage);
 #endif
     }
 
